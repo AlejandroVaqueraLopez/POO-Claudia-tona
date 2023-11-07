3002d2589f05e0c811d3b03ea0223b606d06bad6
Alejandro Vaquera Lopez <alejandrovaqueralopez608@gmail.com>
2023-11-06 20:31:41 -0800

    feat: print and environment variables

---
+++ firstProgram.java
@@ -0,0 +1,10 @@
+public class firstProgram {//the main class must be named as the file
+    public static void main(String[] args){
+        System.out.println("Hello world with new line");
+        System.out.print("Hello world without new line");
+        System.out.println("Hello world on the same line");
+        System.out.println(3);
+        System.out.println(358);
+        System.out.println(50000);
+    }
+}
